3f01d13eeba65bcf26c2799fb460d8db8bdcd6c0
Doppeltje <Doppeltje@users.noreply.github.com>
2019-01-16 18:59:34 +0100

    Update 1

---
+++ Assets/Scripts/KillCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillCounter : MonoBehaviour
+{
+
+	public Text killValue;
+
+	public int value;
+
+    void Start()
+    {
+		value = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+
+	public void AddScore()
+	{
+		value += 1;
+		killValue.text = value.ToString();
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	private Vector2 mousePos;
+	private KillCounter _counter;
+
+    void Start()
+    {
+		_counter = GameObject.Find("Kill count").GetComponent<KillCounter>();
+		Cursor.visible = true;
+		Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//mousePos = Input.mousePosition;
+			//Debug.Log(mousePos);
+			if (Physics.Raycast(ray))
+			{
+				_counter.AddScore();
+				Debug.Log(ray);
+			}
+		}
+    }
+}
