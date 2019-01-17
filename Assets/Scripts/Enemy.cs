3267df30b9891b57c2d6d01c17745e7f8a1bd62a
Thomas Linssen <t.linssen@live.nl>
2019-01-17 14:02:14 +0100

    final

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,4 +15,9 @@ public class Enemy : MonoBehaviour
     {
         
     }
+
+    private void OnDestroy()
+    {
+        
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,24 +7,33 @@ public class Player : MonoBehaviour
 {
 	private Vector2 mousePos;
 	private KillCounter _counter;
+    public GameObject enemy;
+    Vector3 lastPos;
 
     void Start()
     {
 		_counter = GameObject.Find("Kill count").GetComponent<KillCounter>();
 		Cursor.visible = true;
 		Cursor.lockState = CursorLockMode.None;
+        lastPos = enemy.transform.position;
     }
+
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			//mousePos = Input.mousePosition;
-			//Debug.Log(mousePos);
-			if (Physics.Raycast(ray))
+            //mousePos = Input.mousePosition;
+            //Debug.Log(mousePos);
+            RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
 			{
 				_counter.AddScore();
+                Instantiate(enemy, lastPos, Quaternion.identity);
+                lastPos = hit.collider.gameObject.transform.position;
+                Destroy(hit.collider.gameObject);
+
 				Debug.Log(ray);
 			}
 		}
